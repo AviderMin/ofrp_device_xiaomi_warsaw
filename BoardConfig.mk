1188d7f8d33791bfb6cea3c05750c3cb3ecf2d53
AviderMin <gms2917466108@gmail.com>
2026-07-09 14:21:22 +0800

    build: 更新设备配置与vintf清单
    移除了输入黑名单配置，新增所需的aidl硬件抽象层声明

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,6 @@ TW_USE_TOOLBOX := true
 TARGET_USES_MKE2FS := true
 TW_INCLUDE_FUSE_EXFAT := true
 TW_INCLUDE_FUSE_NTFS := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_EXTRA_LANGUAGES := true
